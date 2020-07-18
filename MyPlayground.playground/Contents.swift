3565ac6cbbdad34b700032d6ac2fc465fa171bf2
Junya Kengo <kogepan6852@yahoo.co.jp>
2020-07-18 09:03:40 +0900

    switch編

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,12 +1,15 @@
-// 10日目 Enum編
-// 9日目の正解は「nil」でした
+// 11日目 switch編
+// 10日目の正解は「sunny」でした
 import UIKit
 
-enum Weather: String {
-    case sunny = "sunny"
-    case cloudy = "cloudy"
-    case rainy = "rainy"
+let baseball = ""
+switch baseball {
+case "打つ":
+    print("ヒット！")
+case "投げる":
+    print("いいストレート！")
+case "掴む","キャッチ":
+    print("いいミット音")
+default:
+    print("何もしない")
 }
-
-print(Weather.cloudy.rawValue)//cloudy
-print(Weather.sunny.rawValue)//← 今日の問題
