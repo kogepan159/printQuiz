3dbba9bb89773880150bbacf2c4fe6313bc91a8a
Junya Kengo <kogepan6852@yahoo.co.jp>
2020-07-18 09:46:50 +0900

    14日目の対応

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,9 +1,14 @@
-// 12日目 for in編
-// 11日目の正解は「何もしない」でした
+// 14日目 Optional編(?,!について)
+// 13日目の正解は「あかさたな」でした
 import UIKit
 
-var point: Int = 0
-for num in 0...10 {
-    point = point + 1
-}
-print(point)
+let text: String? = nil
+let text2: String! = "String型について"
+var setText: String = ""
+
+// setText = text ← String型にnilを入れようとするとErrorになる
+setText = text ?? "" //nilだった場合に空文字を入れる
+
+setText = text2 // String型にString型を入れるのでOK
+print(setText)
+
