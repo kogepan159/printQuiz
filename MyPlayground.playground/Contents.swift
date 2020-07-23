b724dab03af179f7179f2b07823fe7c3edb125af
Junya Kengo <kogepan6852@yahoo.co.jp>
2020-07-24 06:36:54 +0900

    // 19日目 break編

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,24 +1,12 @@
-// 18日目 Map/Filter編
-// 17日目の正解は「30」でした
+// 19日目 break編
+// 18日目の正解は「[2, 4]」でした
 import UIKit
 
-let array = [1,2,3,4,5]
-let newArray = array.map { $0 * 2 } // [2,4,6,8,10]
-print(newArray.filter{$0 < 5})
-
-
-
-
-
-
-
-
-
 var count = 0
-
 for _ in 0...4 {
     for _ in 0...5 {
         count = count + 1
+        break
     }
 }
 
