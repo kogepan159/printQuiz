3512b9defb764cff46817a0434cfe5757afb57ae
Junya Kengo <kogepan6852@yahoo.co.jp>
2020-07-24 06:19:11 +0900

    guard編

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -2,12 +2,24 @@
 // 15日目の正解は「test:  111」でした
 import UIKit
 
-let text: String? = "111"
+let text: String? = "aaa"
 let text2: String? = nil
 
-if let ifletText: String = text {
-    print("test: ", ifletText)
-} else if let ifletText2: String = text2 {
-    print("test2: ", ifletText2)
+class GuardTest {
+    static func nilCheck() {
+        guard let guardText: String = text else  {
+            print("guardText nil")
+            return
+        }
+        guard let guardText2: String = text2 else {
+            print("guardText2 nil")
+            return
+        }
+        print("OK nil チェック")
+    }
 }
+
+GuardTest.nilCheck()
+
+
 
