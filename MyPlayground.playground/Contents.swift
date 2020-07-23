323abf75f25ad91284ac6fe5f98fcc6955fb693a
Junya Kengo <kogepan6852@yahoo.co.jp>
2020-07-24 06:33:49 +0900

    Map/Filter編

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,6 +1,18 @@
-// 17日目 二重for編
-// 16日目の正解は「guardText2 nil」でした
+// 18日目 Map/Filter編
+// 17日目の正解は「30」でした
 import UIKit
+
+let array = [1,2,3,4,5]
+let newArray = array.map { $0 * 2 } // [2,4,6,8,10]
+print(newArray.filter{$0 < 5})
+
+
+
+
+
+
+
+
 
 var count = 0
 
