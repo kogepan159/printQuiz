643c1b3e2b93789cb5044eec87400b18f24d3efc
kogepan <>
2020-08-01 08:59:45 +0900

    // 35日目 三項演算

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,30 +1,29 @@
-// 34日目 extension
-// 33日目の正解は「5400」でした
+// 35日目 三項演算
+// 34日目の正解は「2500」でした
 import UIKit
 import Foundation
 
-
-extension Wallet {
-    func chargePasumomo(charge: Int) {
-        pasumomo = pasumomo + charge
-    }
-    
-    func chargeSuicaca(charge: Int) {
-        suicaca = suicaca + charge
-    }
-}
 
 class Wallet {
     var pasumomo: Int = 500
     var suicaca: Int = 400
     
     func autoCharge() {
-        chargePasumomo(charge: 2000)
-        chargeSuicaca(charge: 5000)
+        pasumomo < 500 ?  chargePasumomo(charge: 2000): print("NotCharge")
+        suicaca < 500 ?  chargeSuicaca(charge: 5000) : print("NotCharge")
+
+    }
+    
+    private func chargePasumomo(charge: Int) {
+        pasumomo = pasumomo + charge
+    }
+    
+    private func chargeSuicaca(charge: Int) {
+        suicaca = suicaca + charge
     }
 }
 
 let wallet: Wallet = Wallet()
 wallet.autoCharge()
-print(wallet.pasumomo)
+print(wallet.pasumomo) // ← 今日の問題はこちら
 
