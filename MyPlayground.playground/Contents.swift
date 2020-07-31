ecd7b4acf9d38fa27add9708f8062f943ddeabfd
kogepan <>
2020-07-31 11:03:29 +0900

    28日目 delegate編

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,42 +1,50 @@
-// 27日目 Closure編
-// 26日目の正解は「アイス」でした
+// 28日目 delegate編
+// 27日目の正解は「false」でした
 import UIKit
 
 
-class CanBuy {
-    let brink = { (money: Int) -> Bool in
-        return 150 <= money
+protocol MenuDelegate {
+    func canBay(isBuy: Bool)
+}
+
+class Menu {
+    var delegate:MenuDelegate? = nil
+    private let brinkPrice: Int = 150
+    private let lunchPrice: Int = 1000
+    
+    func brink(money: Int) {
+        delegate?.canBay(isBuy: money >= brinkPrice)
     }
     
-    let lunch = { (money: Int) -> Bool in
-        return 1000 <= money
+    func lunch(money: Int) {
+        delegate?.canBay(isBuy: money >= lunchPrice)
     }
 }
-let walletMoney : Int = 300
 
-let canBuy: CanBuy = CanBuy()
-canBuy.brink(walletMoney)
-print(canBuy.lunch(walletMoney))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-class Wallet {
-    let walletMoney : Int = 300
+class Wallet: MenuDelegate {
+    let menu: Menu = Menu()
+    var walletMoney : Int = 300
+    
+    init() {
+        menu.delegate = self
+    }
+    
+    func canBrink() {
+        menu.brink(money: walletMoney)
+    }
+    
+    func canLunch() {
+        menu.lunch(money: walletMoney)
+    }
+    
+    func canBay(isBuy: Bool) {
+        if isBuy {
+            print("Can buy")
+        } else {
+            print("Not enough money")
+        }
+    }
 }
+
+let wallet: Wallet = Wallet()
+wallet.canLunch()
