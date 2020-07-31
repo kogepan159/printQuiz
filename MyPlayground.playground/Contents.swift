4e492242896527cd2dc01dc7d51376e2f61610b6
kogepan <>
2020-08-01 08:26:11 +0900

    32日目 willSet/didSet

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,17 +1,19 @@
-// 31日目 do-catch
-// 30日目の正解は「1人目の温め完了」でした
+// 32日目 willSet/didSet
+// 31日目の正解は「OK」でした
 import UIKit
 
-func notEmpty(text: String) throws {
-    if text.isEmpty {
-        throw NSError(domain: "空文字です", code: -1, userInfo: ["textEmpty": true])
+// didSetでoldValueを使うって便利ですね
+class Wallet {
+    var money : Int = 300 {
+        willSet {
+            print("willSet")
+        }
+        didSet {
+            print(oldValue)
+        }
     }
 }
 
-do {
-    try notEmpty(text: "空文字です")
-    print("OK")
-} catch {
-    // エラーが発生した場合の処理
-    print(error.localizedDescription)
-}
+let wallet: Wallet = Wallet()
+wallet.money = 1000
+
