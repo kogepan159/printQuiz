33cf0e45f63af704f4828d301ac2b1c5f49cc986
kogepan <>
2020-07-31 09:32:18 +0900

    // 26日目 struct編

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,55 +1,30 @@
-// 25日目 protocol編
-// 24日目の正解は「こげぱん　アイス」でした
+// 26日目 struct編
+// 25日目の正解は「 」でした
 import UIKit
 
-// functionやテストを定義しておくこと
-protocol UniqueAnimalName {
-    var uniqueName: String { get set }
-    var ownerName: String { get set }
-    var animalName: String { get set }
-    func uniqueName(ownerName: String,dogName: String)
-    func getOwnerName() -> String?
-    func getAnimalName() -> String?
-}
-
-class Cat: UniqueAnimalName {
-    var animalName: String = ""
+// classではない、ひとまとまりにしたい時に使う
+struct UniqueAnimalName {
     var uniqueName: String = ""
     var ownerName: String = ""
-    
-    func uniqueName(ownerName: String, dogName: String) {
-    }
-    
-    func getOwnerName() -> String? {
-        return ""
-    }
-    
-    func getAnimalName() -> String? {
-        return ""
-    }
-}
-
-class Dog : UniqueAnimalName {
     var animalName: String = ""
-    var uniqueName: String = ""
-    var ownerName: String = ""
     
-    func uniqueName(ownerName: String,dogName: String) {
+    
+    mutating func setUniqueName(ownerName: String, dogName: String) {
         self.ownerName = ownerName
         self.animalName = dogName
         self.uniqueName = ownerName + " " + dogName
     }
     
-    func getOwnerName() -> String? {
-        return self.ownerName
+    func getOwnerName() -> String {
+        return ownerName
     }
     
-    func getAnimalName() -> String? {
-        return self.animalName
+    func getAnimalName() -> String {
+        return animalName
     }
 }
 
-let dog:Dog = Dog()
-dog.uniqueName(ownerName: "こげぱん", dogName: "アイス")
-print(Cat().uniqueName)
+var name:UniqueAnimalName = UniqueAnimalName()
+name.setUniqueName(ownerName: "こげぱん", dogName: "アイス")
+print(name.getAnimalName())
 
