b5362da24898076b0de3616fecff4fd7a89be238
kogepan <>
2020-08-01 09:05:07 +0900

    // 36日目 少数の数制御

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,29 +1,9 @@
-// 35日目 三項演算
-// 34日目の正解は「2500」でした
+// 36日目 少数の数制御
+// 35日目の正解は「500」でした
 import UIKit
-import Foundation
 
+let oneHundredRunTime: Double = 16.782
 
-class Wallet {
-    var pasumomo: Int = 500
-    var suicaca: Int = 400
-    
-    func autoCharge() {
-        pasumomo < 500 ?  chargePasumomo(charge: 2000): print("NotCharge")
-        suicaca < 500 ?  chargeSuicaca(charge: 5000) : print("NotCharge")
-
-    }
-    
-    private func chargePasumomo(charge: Int) {
-        pasumomo = pasumomo + charge
-    }
-    
-    private func chargeSuicaca(charge: Int) {
-        suicaca = suicaca + charge
-    }
-}
-
-let wallet: Wallet = Wallet()
-wallet.autoCharge()
-print(wallet.pasumomo) // ← 今日の問題はこちら
-
+print(Int(oneHundredRunTime))
+print(String(oneHundredRunTime))
+print(String(format: "%.2f", oneHundredRunTime)) // ←今日の問題
