75614d0fe2edcc5827fe67aa82f5853be055d051
Junya Kengo <kogepan6852@yahoo.co.jp>
2020-08-09 17:56:25 +0900

    // 45日目 自動販売機(お釣り枚数)

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,32 +1,28 @@
-// 44日目 入場料問題
-// 43日目の正解は「20.0」でした
+// 45日目 自動販売機(お釣り枚数)
+// 44日目の正解は「1900」でした
 import UIKit
 
-/*
- 【前提条件】
- 動物園で、大人500円と子供300円の入場料でした、
- 20名以上だと、団体割引で1割引きになります。
- */
 
-class AnimalParkPrice {
-    private let adultPrice: Int = 500
-    private let childrenPrice: Int = 300
-    var adult: Int = 0
-    var children: Int = 0
-    
-    func getTotalPrice() -> Int {
-        let totalPrice: Int = adultPrice * adult + childrenPrice * children
-        if adult + children > 20 {
-            return Int(Double(totalPrice) * 0.9)
-        } else {
-             return totalPrice
+class VendingMachine {
+    let coinPriceList: [Int] = [500,100,50,10]
+    func changeCoinCount(inputMoney: Int, buyMoney: Int) -> Int {
+        var coin: Int = 0
+        var balance: Int = inputMoney - buyMoney
+        if inputMoney < buyMoney {
+            print("買えませんでした。")
+            return coin
         }
+        balance = balance%1000
+        
+        for coinPrice in coinPriceList {
+            if balance/coinPrice > 0 {
+                coin = coin + balance/coinPrice
+                balance = balance%coinPrice
+            }
+        }
+        return coin
     
     }
-    
-    
 }
-let price: AnimalParkPrice = AnimalParkPrice()
-price.adult = 2
-price.children = 3
-print(price.getTotalPrice())
+let price: VendingMachine = VendingMachine()
+print(price.changeCoinCount(inputMoney: 1000, buyMoney: 460))
