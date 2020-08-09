844265671fb0456d0722e000740a903acad68096
Junya Kengo <kogepan6852@yahoo.co.jp>
2020-08-09 18:45:23 +0900

    Storyboard作成対応

---
+++ printQuiz/printQuiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  printQuiz
+//
+//  Created by Junya Kengo on 2020/08/09.
+//  Copyright © 2020 Junya Kengo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
