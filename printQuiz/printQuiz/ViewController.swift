ed419b08d28e810c02e20b10e78f616cd3fe0aa8
Junya Kengo <kogepan6852@yahoo.co.jp>
2020-08-09 20:55:31 +0900

    // 47日目 UIViewController

--- printQuiz/printQuiz/ViewController.swift
+++ printQuiz/printQuiz/ViewController.swift
@@ -1,5 +1,5 @@
-// 46日目 UIViewControllerのライフサイクル
-// 45日目の正解は「5」でした
+// 47日目 UIViewController
+// 46日目の正解は「viewDidAppear」でした
 
 
 // 前提条件: このViewControllerを開いた時の処理
@@ -8,27 +8,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("viewDidLoad")
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewDidDisappear(animated)
-        print("viewWillAppear")
-    }
-
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        print("viewDidAppear")
-    }
-
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        print("viewWillDisappear")
-    }
-    
-    override func viewDidDisappear(_ animated: Bool) {
-        super.viewDidDisappear(animated)
-        print("viewDidDisappear")
+        print(self.view.frame.minX)
     }
 
 }
