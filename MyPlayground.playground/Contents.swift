d8cf80167ce351d5bd53bd2e4156aae89da17f99
Junya Kengo <kogepan6852@yahoo.co.jp>
2020-08-09 17:25:51 +0900

    // 43日目 小学校算数問題

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,29 +1,36 @@
-// 42日目 カウントダウン追加
-// 41日目の正解は「3」でした
+// 43日目 小学校算数問題
+// 42日目の正解は「3」でした
 import UIKit
 
-class CountManagement {
-    private var count: Int = 0
+/*
+ 【算数の問題】
+ Aさんは80m/分で、1600m歩きました。
+ 何分で歩いたでしょうか？
+ 【公式】
+ 道のり = 速さ*時間
+ 速さ = 道のり/時間
+ 時間 = 道のり/速さ
+ */
+
+class OfficialManagement {
+    var distance: Double = 0
+    var speed: Double = 0
+    var time: Double = 0
     
-    func countUp() {
-        count += 1
+    func retrunDistance() -> Double {
+       return speed*time
     }
     
-    func countDown() {
-        count -= 1
+    func retrunSpeed() -> Double {
+        return distance/time
     }
     
-    func nowCount() -> Int {
-        return count
+    func retrunTime() -> Double {
+        return distance/speed
     }
 }
 
-let count: CountManagement = CountManagement()
-count.countDown()
-count.countUp()
-count.countUp()
-count.countUp()
-count.countDown()
-count.countUp()
-count.countDown()
-print(count.nowCount())
+let answer: OfficialManagement = OfficialManagement()
+answer.distance = 1600
+answer.speed = 80
+print(answer.retrunTime())
