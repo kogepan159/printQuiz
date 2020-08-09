fab5dd9e195c78b1e6dd69ade47aee4e3e46b9c4
Junya Kengo <kogepan6852@yahoo.co.jp>
2020-08-09 20:53:37 +0900

    // 46日目 UIViewControllerのライフサイクル

--- printQuiz/printQuiz/ViewController.swift
+++ printQuiz/printQuiz/ViewController.swift
@@ -1,20 +1,35 @@
-//
-//  ViewController.swift
-//  printQuiz
-//
-//  Created by Junya Kengo on 2020/08/09.
-//  Copyright © 2020 Junya Kengo. All rights reserved.
-//
+// 46日目 UIViewControllerのライフサイクル
+// 45日目の正解は「5」でした
 
+
+// 前提条件: このViewControllerを開いた時の処理
 import UIKit
-
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("viewDidLoad")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("viewWillAppear")
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("viewDidAppear")
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("viewWillDisappear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("viewDidDisappear")
+    }
 
 }
 
