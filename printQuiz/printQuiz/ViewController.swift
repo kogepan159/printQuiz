48be3344d33414626043f945f7f4ab1fd17f5a91
Junya Kengo <kogepan6852@yahoo.co.jp>
2020-08-10 09:45:27 +0900

    // 48日目 UIButton

--- printQuiz/printQuiz/ViewController.swift
+++ printQuiz/printQuiz/ViewController.swift
@@ -1,14 +1,16 @@
-// 47日目 UIViewController
-// 46日目の正解は「viewDidAppear」でした
-
+// 48日目 UIButton
+// 47日目の正解は「0」でした
 
 // 前提条件: このViewControllerを開いた時の処理
 import UIKit
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var quizButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(self.view.frame.minX)
+        print(quizButton.tag)
     }
 
 }
