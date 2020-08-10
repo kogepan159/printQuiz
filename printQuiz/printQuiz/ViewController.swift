bf67b18cef5d2526b9d8699fdaf7c0fd95787127
Junya Kengo <kogepan6852@yahoo.co.jp>
2020-08-10 10:04:29 +0900

    // 50日目 UISegment

--- printQuiz/printQuiz/ViewController.swift
+++ printQuiz/printQuiz/ViewController.swift
@@ -1,25 +1,18 @@
-// 49日目 sender
-// 48日目の正解は「1」でした
+// 50日目 UISegment
+// 49日目の正解は「同じである」でした
 
 // 前提条件: このViewControllerを開いた時の処理
 import UIKit
 class ViewController: UIViewController {
     
     
+    @IBOutlet weak var quizSegment: UISegmentedControl!
     @IBOutlet weak var quizButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        print(quizSegment.selectedSegmentIndex)
     }
 
-    @IBAction func tapButton(_ sender: UIButton) {
-        
-        if sender == quizButton {
-            print("同じである")
-        } else {
-            print("同じでない")
-        }
-        
-    }
 }
 
