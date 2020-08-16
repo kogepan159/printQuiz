6ec5a02074e261075f54576fe7374c780e5735f8
Junya Kengo <kogepan6852@yahoo.co.jp>
2020-08-16 10:56:50 +0900

    // 51日目 TableView

--- printQuiz/printQuiz/ViewController.swift
+++ printQuiz/printQuiz/ViewController.swift
@@ -1,18 +1,50 @@
-// 50日目 UISegment
-// 49日目の正解は「同じである」でした
+// 51日目 TableView
+// 50日目の正解は「0」でした
 
 // 前提条件: このViewControllerを開いた時の処理
 import UIKit
 class ViewController: UIViewController {
     
-    
     @IBOutlet weak var quizSegment: UISegmentedControl!
     @IBOutlet weak var quizButton: UIButton!
+    @IBOutlet weak var tableview: UITableView!
+    let fruitsArray = ["みかん","もも", "バナナ", "リンゴ", "すいか"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(quizSegment.selectedSegmentIndex)
+        tableview.delegate = self
+        tableview.dataSource = self
+        tableview.reloadData()
+        tableview.tableFooterView = UIView()
     }
-
 }
 
+
+extension ViewController:  UITableViewDelegate, UITableViewDataSource {
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return fruitsArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 40
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = fruitsArray[indexPath.row]
+        if indexPath.row == 2 {
+            print(fruitsArray[indexPath.row])
+        }
+        
+        return cell
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // タップされた時の処理
+    }
+    
+}
