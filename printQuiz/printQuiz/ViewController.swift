cfd82ba405827b845a311e7ad0258363bdf2d8a8
Junya Kengo <kogepan6852@yahoo.co.jp>
2020-08-27 10:24:44 +0900

    // 68日目 キーボード入力

--- printQuiz/printQuiz/ViewController.swift
+++ printQuiz/printQuiz/ViewController.swift
@@ -1,5 +1,5 @@
-// 67日目 セグメントアクション
-// 66日目の正解は「0」でした
+// 68日目 キーボード入力
+// 67日目の正解は「2」でした
 
 // 前提条件: このViewControllerを開いた時の処理
 import UIKit
@@ -8,50 +8,48 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var quizSegment: UISegmentedControl!
     @IBOutlet weak var quizButton: UIButton!
-    @IBOutlet weak var tableview: UITableView!
     @IBOutlet weak var quizLabel: UILabel!
+    @IBOutlet weak var quizTextField: UITextField!
     let fruitsArray = ["みかん","もも", "バナナ", "リンゴ", "すいか"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableview.delegate = self
-        tableview.dataSource = self
-        tableview.reloadData()
-        tableview.tableFooterView = UIView()
-    }
-    
-    @IBAction func segmentedControlChanged(_ sender: UISegmentedControl) {
-        // テストをタップした時
-        print(sender.selectedSegmentIndex)
-    }
-}
-
-
-extension ViewController:  UITableViewDelegate, UITableViewDataSource {
-
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return "果物リスト"
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return fruitsArray.count
-    }
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 40
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        cell.textLabel?.text = fruitsArray[indexPath.row] + String(indexPath.row)
-        return cell
-    }
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        // タップされた時の処理(リンゴをタップした時)
-        print(indexPath.section)
+        self.quizTextField.delegate = self
+        NotificationCenter.default.addObserver(forName: UITextField.textDidChangeNotification, object: quizTextField, queue: .main) { (notification) in
+            print(self.quizTextField.text)
+        }
     }
     
 }
+
+extension ViewController: UITextFieldDelegate {
+    /*
+    UITextFieldが編集された直後に呼ばれるデリゲートメソッド.
+    */
+
+    func textFieldDidBeginEditing(_ textField: UITextField){
+
+        print("textFieldDidBeginEditing:" + textField.text!)
+    }
+
+    /*
+    UITextFieldが編集終了する直前に呼ばれるデリゲートメソッド.
+    */
+
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+
+        print("textFieldShouldEndEditing:" + textField.text!)
+        return true
+    }
+
+    /*
+    改行ボタンが押された際に呼ばれるデリゲートメソッド.
+    */
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // キーボードを閉じる処理
+        textField.resignFirstResponder()
+        return true
+    }
+}
+
