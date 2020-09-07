3887220034adefbb0e07f1b2397d88c48672033b
Junya Kengo <kogepan6852@yahoo.co.jp>
2020-09-07 15:20:45 +0900

    // 82日目 UIColor(foregroundColor)

--- quitswiftui/quitswiftui/ContentView.swift
+++ quitswiftui/quitswiftui/ContentView.swift
@@ -1,15 +1,19 @@
-// 81日目 Tap対応
-// 80日目の正解は「Label」でした
+// 82日目 UIColor(foregroundColor)
+// 81日目の正解は「aaaaa」でした
 // 参考URL: https://developer.apple.com/tutorials/swiftui/
 
-//問題: Helloをタップしたときに出てくるものは？？
+//問題: Buttonをタップしたときに出てくるものは？？
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello").onTapGesture {
-            print("aaaaa")
+        VStack {// 縦に並べる
+            Text("Hello2222").foregroundColor(.red).padding(.all, 8.0);
+            Text("Hello2222").foregroundColor(.red).padding(.all, 8.0)
+            Button(action: {print("111")}) {
+                Text(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/)
+            };
         }
     }
 }
