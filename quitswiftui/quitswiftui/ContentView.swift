492126a0aecc85e0c4551b328781156cb7cde76c
Junya Kengo <kogepan6852@yahoo.co.jp>
2020-09-07 15:39:12 +0900

    // 83日目　List(UITableview)

--- quitswiftui/quitswiftui/ContentView.swift
+++ quitswiftui/quitswiftui/ContentView.swift
@@ -1,25 +1,40 @@
-// 82日目 UIColor(foregroundColor)
-// 81日目の正解は「aaaaa」でした
-// 参考URL: https://developer.apple.com/tutorials/swiftui/
+// 83日目　List(UITableview)
+// 82日目の正解は「111」でした
 
-//問題: Buttonをタップしたときに出てくるものは？？
+//問題: 一番上のリストをタップしたら、何がprintされますか？
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {// 縦に並べる
-            Text("Hello2222").foregroundColor(.red).padding(.all, 8.0);
-            Text("Hello2222").foregroundColor(.red).padding(.all, 8.0)
+        VStack {
+            // 1...5は動かないらしいです
+            List(1..<5) { localIndex in
+                listView(localIndex:localIndex)
+            }
+            
             Button(action: {print("111")}) {
-                Text(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/)
-            };
-        }
+                Text("テスト")
+            }.padding()
+        }.padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+// リスト中身サンプル
+struct listView: View {
+    var localIndex: Int = 0
+    var body: some View {
+        HStack {// 横に並べる
+            Text("self.localIndex")
+            Text("self.localIndex2")
+        }.padding().onTapGesture {
+            print(self.localIndex)
+        }
     }
 }
