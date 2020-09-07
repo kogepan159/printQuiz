05eb0c3968274f67774763379b45de17e1480304
Junya Kengo <kogepan6852@yahoo.co.jp>
2020-09-07 15:43:52 +0900

    // 84日目　WKWebView(UIWebView)

--- quitswiftui/quitswiftui/ContentView.swift
+++ quitswiftui/quitswiftui/ContentView.swift
@@ -1,22 +1,14 @@
-// 83日目　List(UITableview)
-// 82日目の正解は「111」でした
+// 84日目　WKWebView(UIWebView)
+// 83日目の正解は「1」でした
 
-//問題: 一番上のリストをタップしたら、何がprintされますか？
+//問題: 何が出力されるでしょうか？
 
 import SwiftUI
+import WebKit
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            // 1...5は動かないらしいです
-            List(1..<5) { localIndex in
-                listView(localIndex:localIndex)
-            }
-            
-            Button(action: {print("111")}) {
-                Text("テスト")
-            }.padding()
-        }.padding()
+        WebView(loadUrl: "https://www.apple.com")
     }
 }
 
@@ -26,15 +18,16 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
-// リスト中身サンプル
-struct listView: View {
-    var localIndex: Int = 0
-    var body: some View {
-        HStack {// 横に並べる
-            Text("self.localIndex")
-            Text("self.localIndex2")
-        }.padding().onTapGesture {
-            print(self.localIndex)
-        }
+
+struct WebView: UIViewRepresentable {
+    var loadUrl:String
+
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        print(loadUrl)
+        uiView.load(URLRequest(url: URL(string: loadUrl)!))
     }
 }
