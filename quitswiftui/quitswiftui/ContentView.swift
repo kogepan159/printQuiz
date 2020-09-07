e9aa3114d9bb48f42c6059c97f020fa4c548b257
Junya Kengo <kogepan6852@yahoo.co.jp>
2020-09-07 16:00:55 +0900

    // 86日目　 端末判定+switch文

--- quitswiftui/quitswiftui/ContentView.swift
+++ quitswiftui/quitswiftui/ContentView.swift
@@ -1,22 +1,25 @@
-// 85日目　OS(OS判定)+if文
-// 84日目の正解は「https://www.apple.com」でした
+// 86日目　 端末判定+switch文
+// 85日目の正解は「OS13.0以外 13.6」でした
 
-//問題: OS13.6の時に、どのように表示されますか？
+//問題: phoneの時に、buildすると何が表示されますか？
 
 import SwiftUI
 import WebKit
 
 struct ContentView: View {
     var body: some View {
-        if UIDevice.current.systemVersion == "13.0" {
+        switch UIDevice.current.userInterfaceIdiom  {
+        case .phone:
             return HStack {
-                Text("OS13.0")
-                Text(UIDevice.current.systemVersion)
+                Text("吾輩はiPhoneである")
             }
-        } else {
+        case .pad:
             return HStack {
-                Text("OS13.0以外")
-                Text(UIDevice.current.systemVersion)
+                Text("吾輩はiPadである")
+            }
+        default:
+            return HStack {
+                Text("iPhone/iPadじゃない")
             }
         }
         
