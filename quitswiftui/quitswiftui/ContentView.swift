7fc6567f71bdc07cd7009532cf658bcf656aeb13
Junya Kengo <kogepan6852@yahoo.co.jp>
2020-09-07 14:42:13 +0900

    SwiftUI雛形

---
+++ quitswiftui/quitswiftui/ContentView.swift
@@ -0,0 +1,18 @@
+// 81日目 ダークモード
+// 80日目の正解は「Label」でした
+
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
